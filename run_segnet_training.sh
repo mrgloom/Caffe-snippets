d0abadd6b17631716693672ecf576df5db063984
mrgloom <gloomyviking@gmail.com>
2016-04-01 13:25:01 +0400

    Create run_segnet_training.sh

---
+++ run_segnet_training.sh
@@ -0,0 +1,4 @@
+#for some reason can't add to LD_LIBRARY_PATH so run it this way
+
+
+/home/user/Downloads/caffe-segnet/build/tools/caffe train -solver /home/user/Downloads/SegNet-Tutorial/Models/segnet_simple_solver.prototxt
